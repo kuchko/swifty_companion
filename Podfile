fe221d22acc7c1901d3f5878c17be655607a1a27
Oleksii KUCHKO <okuchko@e1r7p17.unit.ua>
2019-08-29 15:30:23 +0300

    betta 1. Start Almofire and SwiftyJSON loaded

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'swifty_companion' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for swifty_companion
+	pod 'Alamofire'
+	pod 'SwiftyJSON'
+end
---
+++ swifty_companion/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  swifty_companion
+//
+//  Created by Oleksii KUCHKO on 8/27/19.
+//  Copyright © 2019 Oleksii KUCHKO. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
